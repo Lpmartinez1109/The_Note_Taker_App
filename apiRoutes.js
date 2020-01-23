e15c5af257823f30499398e211ccc853eb84a4d4
LPMartinez <lpmartinez1109@gmail.com>
2020-01-23 11:49:38 -0600

    Created files for use

---
+++ apiRoutes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const store = require("./store");
+
+function Router() {
+    router.get("/notes", function (req, res) {
+        store
+            .getNotes()
+            .then(notes => res.json(notes))
+            .catch(err => res.status(500).json(err));
+
+    });
+
+    router.post("/notes", (req, res) => {
+        store
+            .addNotes(req.body)
+            .then((note) => res.json(note))
+            .catch(err => res.status(500).json(err));
+    });
+
+
+    router.delete("/notes/:id", function (req, res) {
+        store
+            .deleteNotes(req.body, id)
+            .then((note) => res.json(note))
+            .catch(err => res.status(500).json(err));
+    })
+}
+module.exports = Router;
