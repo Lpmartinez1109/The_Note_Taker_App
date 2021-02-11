6d459ce2a5510573373f09889820a6d21e1f3009
LPMartinez <lpmartinez1109@gmail.com>
2021-02-10 21:04:00 -0600

    Updated server.js file

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const apiRoutes = require("./apiRoutes");
-const htmlRoutes = require("./htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-app.listen(PORT, function(){ 
-    console.log('Listening on on : http://localhost:' + PORT)
-});
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
